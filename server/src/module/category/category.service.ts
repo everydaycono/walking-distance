0cb08a01a741efd85a47e1b52828033cb9b79a3b
minji <321963741@naver.com>
2023-09-06 22:42:13 +0900

    feat: create category service logic

--- server/src/module/category/category.service.ts
+++ server/src/module/category/category.service.ts
@@ -1,16 +1,47 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from "@nestjs/common";
 import { Category } from "./category.entity";
 import { Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
 
 @Injectable()
 export class CategoryService {
-  constructor(private readonly categoryRepository: Repository<Category>) {}
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>
+  ) {}
   /**
    * create a new category
    */
   async create(category: Partial<Category>) {
-    return {
-      msg: "create a new category",
-    };
+    const { label } = category;
+
+    // require label
+    if (!label) {
+      throw new BadRequestException("Label is required");
+    }
+
+    // find category by label
+    const existCategory = await this.categoryRepository.findOne({
+      where: {
+        label,
+      },
+    });
+
+    // if exist  throw error
+    if (existCategory) {
+      throw new HttpException(
+        "Category already exists",
+        HttpStatus.BAD_REQUEST
+      );
+    }
+
+    const newCategory = await this.categoryRepository.create(category);
+    await this.categoryRepository.save(newCategory);
+    return { newCategory };
   }
 }
