cd3d53fc1ad34b1a5b4cdb382678dc80e5a7d07e
minji <321963741@naver.com>
2023-09-07 15:46:11 +0900

    feat: create tag logic

--- server/src/module/tag/tag.controller.ts
+++ server/src/module/tag/tag.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { RolesGuard } from '../auth/guard/roles.guard';
+import { TagService } from './tag.service';
+import { Tag } from './tag.entity';
+import { Roles } from '../auth/decorators/roles.decorators';
+import { Role } from '../auth/role.enum';
+import { JwtGuard } from '../auth/guard/access-jwt.guard';
 
+@ApiTags('Tag')
 @Controller('tag')
-export class TagController {}
+@UseGuards(RolesGuard)
+export class TagController {
+  constructor(private readonly tagService: TagService) {}
+
+  /**
+   * create tag
+   */
+  @ApiResponse({ status: 201, description: 'create tag', type: [Tag] })
+  @Post()
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtGuard)
+  create(@Body() tag) {
+    return this.tagService.create(tag);
+  }
+}
--- server/src/module/tag/tag.service.ts
+++ server/src/module/tag/tag.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Tag } from './tag.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TagService {}
+export class TagService {
+  constructor(
+    @InjectRepository(Tag) private readonly tagRepository: Repository<Tag>
+  ) {}
+  /**
+   * create tag
+   */
+  async create(tag) {
+    const { label } = tag;
+
+    // find tag by label
+    const existTag = await this.tagRepository.findOne({ where: { label } });
+
+    // if exist  throw error
+    if (existTag) {
+      throw new BadRequestException(` ${label}Tag already exists`);
+    }
+
+    const newTag = await this.tagRepository.create(tag);
+    await this.tagRepository.save(newTag);
+
+    return newTag;
+  }
+}
