f022dbe9dd93be9a712ce0211b36d2df7e3be15f
minji <321963741@naver.com>
2023-09-08 15:28:02 +0900

    fix: config에서 entity import해오던 방식 일단 각각 entity파일에서 Import 해오는 방식으로 변경

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './module/user/user.module';
 import { AuthModule } from './module/auth/auth.module';
-import { config } from './config';
 import { ConfigModule } from '@nestjs/config';
 import { MailModule } from './module/mail/mail.module';
 import { AwsModule } from './module/oss/oss.module';
@@ -10,8 +9,11 @@ import { ArticleModule } from './module/article/article.module';
 import { CategoryModule } from './module/category/category.module';
 import { CommentModule } from './module/comment/comment.module';
 import { TagModule } from './module/tag/tag.module';
-
-const { User, Article, Category, Tag, Comment } = config.dbEntity;
+import { Article } from './module/article/article.entity';
+import { Category } from './module/category/category.entity';
+import { Comment } from './module/comment/entities/comment.entity';
+import { Tag } from './module/tag/tag.entity';
+import { User } from './module/user/user.entity';
 
 @Module({
   imports: [
