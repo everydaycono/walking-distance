714ce5036ce49eefae8fe68794bce79126c4e064
minji <321963741@naver.com>
2023-09-08 11:45:45 +0900

    refactor: api swagger 수정 및 create return type changed

--- server/src/module/tag/tag.controller.ts
+++ server/src/module/tag/tag.controller.ts
@@ -3,11 +3,12 @@ import {
   Controller,
   Get,
   Param,
+  Patch,
   Post,
   Query,
   UseGuards
 } from '@nestjs/common';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { RolesGuard } from '../auth/guard/roles.guard';
 import { TagService } from './tag.service';
 import { Tag } from './tag.entity';
@@ -25,16 +26,19 @@ export class TagController {
    * create tag
    */
   @ApiResponse({ status: 201, description: 'create tag', type: [Tag] })
+  @ApiOperation({ summary: 'Create Tag' })
   @Post()
   @Roles(Role.ADMIN)
   @UseGuards(JwtGuard)
-  create(@Body() tag: Partial<Tag>): Promise<Tag> {
+  create(@Body() tag: Partial<Tag>) {
     return this.tagService.create(tag);
   }
 
   /**
    * find all tag
    */
+  @ApiResponse({ status: 200, description: 'find all tag', type: [Tag] })
+  @ApiOperation({ summary: 'Find all Tags' })
   @Get()
   findAll(@Query() queryParams): Promise<Tag[]> {
     return this.tagService.findAll(queryParams);
@@ -43,6 +47,8 @@ export class TagController {
   /**
    * find single tag
    */
+  @ApiResponse({ status: 200, description: 'find single tag', type: [Tag] })
+  @ApiOperation({ summary: 'Find single Tag' })
   @Get(':id')
   findById(@Param('id') id: string) {
     return this.tagService.findById(id);
