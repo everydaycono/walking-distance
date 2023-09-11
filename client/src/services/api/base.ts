e9700f5bcad912258e3accd994df3a87446f272b
minji <321963741@naver.com>
2023-09-11 23:59:23 +0900

    refactor: base api code fix

--- client/src/services/api/base.ts
+++ client/src/services/api/base.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL;
+const baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL;
 const isServer = typeof window === 'undefined';
 
 const api = axios.create({
@@ -10,25 +10,4 @@ const api = axios.create({
   }
 });
 
-// api.interceptors.request.use(async (config) => {
-//   if (isServer) {
-//     const { cookies } = await import('next/headers'),
-//       token = cookies().get('token')?.value;
-
-//     if (token) {
-//       config.headers['Authorization'] = `Bearer ${token}`;
-//     }
-//   } else {
-//     const token = document.cookie.replace(
-//       /(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/,
-//       '$1'
-//     );
-
-//     if (token) {
-//       config.headers['Authorization'] = `Bearer ${token}`;
-//     }
-//   }
-
-//   return config;
-// });
 export default api;
