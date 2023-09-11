5a49ce64d1238572ebc98a4c6db80325aac888de
minji <321963741@naver.com>
2023-09-11 17:52:42 +0900

    feat: articleValidator logic with zod

---
+++ client/src/lib/validators/article.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+
+export const articleValidator = z.object({
+  title: z.string(),
+  //   content: z.string().max(3000, 'Content must be less than 3000 characters'),
+  tags: z.string(),
+  category: z.string()
+});
