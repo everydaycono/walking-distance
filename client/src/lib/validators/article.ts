50b9ce984fabe5231c031ffb9e846c860140c1d2
minji <321963741@naver.com>
2023-09-12 14:25:36 +0900

    feat: customCategory zod added

--- client/src/lib/validators/article.ts
+++ client/src/lib/validators/article.ts
@@ -2,7 +2,9 @@ import { z } from 'zod';
 
 export const articleValidator = z.object({
   title: z.string(),
-  //   content: z.string().max(3000, 'Content must be less than 3000 characters'),
+  content: z.string().max(3000, 'Content must be less than 3000 characters'),
   tags: z.string(),
-  category: z.string()
+  category: z.string(),
+  // this only aviliable when category is custom
+  customCategory: z.string().optional()
 });
