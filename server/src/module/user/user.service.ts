19430c9454c927c7f840906a6dc05b85d9fef842
minji <321963741@naver.com>
2023-09-13 17:26:35 +0900

    feat: getMyArticles, getOtherArticles controller, service init

--- server/src/module/user/user.service.ts
+++ server/src/module/user/user.service.ts
@@ -1,11 +1,11 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { AwsService } from '../oss/oss.service';
 
 @Injectable()
 export class UserService {
   constructor(private awsService: AwsService) {}
+
+  // update avatar
   async updateAvatar(dataBuffer: Buffer, filename: string, fileType: string) {
     try {
       const uploadResult = await this.awsService.uploadPublicFile(
@@ -22,19 +22,13 @@ export class UserService {
     }
   }
 
-  findAll() {
-    return `This action returns all user`;
+  // get my articles
+  getMyArticles(userId: string) {
+    return 'get my articles';
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  // get other articles
+  getPersonArticles(userId: string) {
+    return 'get other articles';
   }
 }
