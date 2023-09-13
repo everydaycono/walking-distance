c217a94eeeaf40adbffa59414306b448aea28da8
minji <321963741@naver.com>
2023-09-13 11:34:20 +0900

    fix: type name changed

--- server/src/module/article/types/article.type.ts
+++ server/src/module/article/types/article.type.ts
@@ -1,8 +1,10 @@
 import { Article } from '../article.entity';
 
 type CategoryLabelType = string;
+type TagsType = string[];
 type PartialArtcile = Partial<Article>;
 
-export type CustomArticleType = PartialArtcile & {
+export type InputArticleType = PartialArtcile & {
   category: CategoryLabelType;
+  tags: TagsType;
 };
