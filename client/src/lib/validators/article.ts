869121eec3c9a6f1e381047bd2dcb27b53272ba7
Cono <everydaycono@gmail.com>
2023-09-14 12:38:32 +0900

    feat: reactquill image upload (uploadthing init)

--- client/src/lib/validators/article.ts
+++ client/src/lib/validators/article.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod';
 
 export const articleValidator = z.object({
-  title: z.string(),
-  content: z.string().max(3000, 'Content must be less than 3000 characters'),
+  title: z.string().min(10, 'Title must be at least 10 characters'),
+  // content: z.string().max(3000, 'Content must be less than 3000 characters'),
   tags: z.string(),
-  category: z.string(),
+  category: z.string().min(3, 'Category must be selected')
   // this only aviliable when category is custom
-  customCategory: z.string().optional()
+  // customCategory: z.string().optional()
 });
