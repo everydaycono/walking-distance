464bac65e489beeac84f4948da80df8678a2e00f
Cono <everydaycono@gmail.com>
2023-09-16 01:06:55 +0900

    fix: quill wrapper not appear

--- client/src/components/wrapper/QuillWrapper.tsx
+++ client/src/components/wrapper/QuillWrapper.tsx
@@ -14,38 +14,23 @@ import 'react-quill/dist/quill.snow.css';
 import { uploadFiles } from '@/utils/uploadthing/config';
 import dynamic from 'next/dynamic';
 
-interface IWrappedComponent extends React.ComponentProps<typeof ReactQuill> {
-  forwardedRef: LegacyRef<ReactQuill>;
-}
-const ReactQuillBase = dynamic(
-  async () => {
-    const { default: ReactQuill } = await import('react-quill');
-
-    function QuillJS({ forwardedRef, ...props }: IWrappedComponent) {
-      return <ReactQuill ref={forwardedRef} {...props} />;
-    }
-
-    return QuillJS;
-  },
-  {
-    ssr: false
-  }
-);
-
 interface QuillNoSSRWrapperProps {
   className?: string;
   onChange?: (value: string) => void;
 }
-const QuillWrapper: FC<QuillNoSSRWrapperProps> = ({ onChange, className }) => {
-  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+const QuillNoSSRWrapper: FC<QuillNoSSRWrapperProps> = ({
+  className,
+  onChange
+}) => {
   const quillRef = useRef<ReactQuill>(null);
 
-  const imageHandler = useCallback(async () => {
+  const imageHandler = async () => {
     const input = document.createElement('input');
+
     input.setAttribute('type', 'file');
     input.setAttribute('accept', 'image/*');
     input.click();
-
     input.onchange = async () => {
       const file: File | null = input && input.files ? input.files[0] : null;
       if (quillRef.current && file) {
@@ -64,7 +49,9 @@ const QuillWrapper: FC<QuillNoSSRWrapperProps> = ({ onChange, className }) => {
               );
             }
           });
+
           const range = quillObj.getSelection(true)?.index ?? 0;
+
           const uploadImg = uploadFile[0];
           quillObj.deleteText(range, 1);
           quillObj.insertEmbed(range, 'image', uploadImg.url);
@@ -75,87 +62,77 @@ const QuillWrapper: FC<QuillNoSSRWrapperProps> = ({ onChange, className }) => {
         }
       }
     };
-  }, [quillRef.current]);
-
-  const modules = {
-    toolbar: {
-      container: [
-        //[{ 'font': [] }],
-        [{ header: [1, 2, 3, 4, false] }],
-        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [
-          { list: 'ordered' },
-          { list: 'bullet' },
-          { indent: '-1' },
-          { indent: '+1' }
-        ],
-        ['link', 'image'],
-        [{ align: [] }, { color: [] }, { background: [] }], // dropdown with defaults from theme
-        ['clean']
-      ],
-      handlers: {
-        image: imageHandler
-      }
-    }
   };
-  const formats = [
-    'header',
-    'font',
-    'size',
-    'bold',
-    'italic',
-    'underline',
-    'strike',
-    'blockquote',
-    'list',
-    'bullet',
-    'indent',
-    'link',
-    'image',
-    'video'
-  ];
-  useEffect(() => {
-    if (typeof window === 'undefined') {
-      setIsMounted(true);
-    }
+
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: [
+          //[{ 'font': [] }],
+          [{ header: [1, 2, 3, 4, false] }],
+          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+          [
+            { list: 'ordered' },
+            { list: 'bullet' },
+            { indent: '-1' },
+            { indent: '+1' }
+          ],
+          ['link', 'image'],
+          [{ align: [] }, { color: [] }, { background: [] }], // dropdown with defaults from theme
+          ['clean']
+        ],
+        handlers: {
+          image: imageHandler
+        }
+      }
+    };
   }, []);
 
-  if (!isMounted) return null;
-
   return (
-    <>
-      <div>
-        <ReactQuillBase
-          className="h-96"
-          onChange={onChange}
-          modules={modules}
-          formats={formats}
-          forwardedRef={quillRef}
-        />
-      </div>
-    </>
+    <ReactQuill
+      style={{ height: '500px', maxWidth: '100%' }}
+      ref={quillRef}
+      theme="snow"
+      modules={modules}
+      onChange={onChange}
+    />
   );
 };
 
-export default QuillWrapper;
+export default QuillNoSSRWrapper;
+
+// interface IWrappedComponent extends React.ComponentProps<typeof ReactQuill> {
+//   forwardedRef: LegacyRef<ReactQuill>;
+// }
+// const ReactQuillBase = dynamic(
+//   async () => {
+//     const { default: ReactQuill } = await import('react-quill');
+
+//     function QuillJS({ forwardedRef, ...props }: IWrappedComponent) {
+//       return <ReactQuill ref={forwardedRef} {...props} />;
+//     }
+
+//     return QuillJS;
+//   },
+//   {
+//     ssr: false
+//   }
+// );
 
 // interface QuillNoSSRWrapperProps {
 //   className?: string;
 //   onChange?: (value: string) => void;
 // }
-
-// const QuillNoSSRWrapper: FC<QuillNoSSRWrapperProps> = ({
-//   className,
-//   onChange
-// }) => {
+// const QuillWrapper: FC<QuillNoSSRWrapperProps> = ({ onChange, className }) => {
+//   // const [isMounted, setIsMounted] = useState<boolean>(false);
 //   const quillRef = useRef<ReactQuill>(null);
 
 //   const imageHandler = async () => {
 //     const input = document.createElement('input');
-
 //     input.setAttribute('type', 'file');
 //     input.setAttribute('accept', 'image/*');
 //     input.click();
+
 //     input.onchange = async () => {
 //       const file: File | null = input && input.files ? input.files[0] : null;
 //       if (quillRef.current && file) {
@@ -174,9 +151,7 @@ export default QuillWrapper;
 //               );
 //             }
 //           });
-
 //           const range = quillObj.getSelection(true)?.index ?? 0;
-
 //           const uploadImg = uploadFile[0];
 //           quillObj.deleteText(range, 1);
 //           quillObj.insertEmbed(range, 'image', uploadImg.url);
@@ -189,44 +164,64 @@ export default QuillWrapper;
 //     };
 //   };
 
-//   const modules = useMemo(() => {
-//     return {
-//       toolbar: {
-//         container: [
-//           //[{ 'font': [] }],
-//           [{ header: [1, 2, 3, 4, false] }],
-//           ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-//           [
-//             { list: 'ordered' },
-//             { list: 'bullet' },
-//             { indent: '-1' },
-//             { indent: '+1' }
-//           ],
-//           ['link', 'image'],
-//           [{ align: [] }, { color: [] }, { background: [] }], // dropdown with defaults from theme
-//           ['clean']
+//   const modules = {
+//     toolbar: {
+//       container: [
+//         //[{ 'font': [] }],
+//         [{ header: [1, 2, 3, 4, false] }],
+//         ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+//         [
+//           { list: 'ordered' },
+//           { list: 'bullet' },
+//           { indent: '-1' },
+//           { indent: '+1' }
 //         ],
-//         handlers: {
-//           image: imageHandler
-//         }
+//         ['link', 'image'],
+//         [{ align: [] }, { color: [] }, { background: [] }], // dropdown with defaults from theme
+//         ['clean']
+//       ],
+//       handlers: {
+//         image: imageHandler
 //       }
-//     };
-//   }, []);
+//     }
+//   };
+//   const formats = [
+//     'header',
+//     'font',
+//     'size',
+//     'bold',
+//     'italic',
+//     'underline',
+//     'strike',
+//     'blockquote',
+//     'list',
+//     'bullet',
+//     'indent',
+//     'link',
+//     'image',
+//     'video'
+//   ];
+//   // useEffect(() => {
+//   //   if (typeof window === 'undefined') {
+//   //     setIsMounted(true);
+//   //   }
+//   // }, []);
+
+//   // if (!isMounted) return null;
 
 //   return (
-//     <ReactQuill
-//       style={{ height: '500px', maxWidth: '100%' }}
-//       ref={quillRef}
-//       theme="snow"
-//       modules={modules}
-//       onChange={onChange}
-//     />
+//     <>
+//       <div>
+//         <ReactQuillBase
+//           className="h-96"
+//           onChange={onChange}
+//           modules={modules}
+//           formats={formats}
+//           forwardedRef={quillRef}
+//         />
+//       </div>
+//     </>
 //   );
 // };
 
-// export default QuillNoSSRWrapper;
-
-//
-//
-//
-//
+// export default QuillWrapper;
