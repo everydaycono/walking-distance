34001c2be884e8a6729df03007904939f988c7e4
Cono <everydaycono@gmail.com>
2023-09-20 11:13:33 +0900

    feat: social login github

--- client/src/components/AuthForm.tsx
+++ client/src/components/AuthForm.tsx
@@ -81,6 +81,10 @@ const AuthForm: FC<AuthFormProps> = ({ type }) => {
     }));
   };
 
+  const handleGithubButton = () => {
+    window.location.href = 'http://localhost:8000/api/auth/social-redirect';
+  };
+
   const register = () => {
     if (
       authFormValue.firstName === '' ||
@@ -262,7 +266,24 @@ const AuthForm: FC<AuthFormProps> = ({ type }) => {
                 </div>
               )}
               {signupError && (
-                <h1 className="text-red-500 font-bold">{signupError}</h1>
+                <div
+                  className="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
+                  role="alert"
+                >
+                  <svg
+                    className="flex-shrink-0 inline w-4 h-4 mr-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                  </svg>
+                  <span className="sr-only">Info</span>
+                  <div>
+                    <span className="font-medium">{signupError}</span>
+                  </div>
+                </div>
               )}
               <button
                 onClick={handleAuthSubmitButton}
@@ -276,6 +297,7 @@ const AuthForm: FC<AuthFormProps> = ({ type }) => {
             <button
               type="button"
               className="w-full justify-center text-white bg-[#616871] hover:bg-[#616871]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
+              onClick={handleGithubButton}
             >
               <svg
                 className="w-4 h-4 mr-2"
