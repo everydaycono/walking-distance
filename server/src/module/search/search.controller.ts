51ae7bb650d291a295ff47048d649ec18afb97dd
minji <321963741@naver.com>
2023-09-20 16:54:58 +0900

    docs: swagger 정리

--- server/src/module/search/search.controller.ts
+++ server/src/module/search/search.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, HttpCode, HttpStatus, Query } from '@nestjs/common';
+import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { SearchService } from './search.service';
+import { CategoryType } from './types/category.type';
 
 @ApiTags('🔎 Search')
 @Controller('search')
@@ -10,24 +11,47 @@ export class SearchController {
   /**
    * search article by title or content
    */
+  @ApiOperation({ summary: 'Search Article by Title or Content' })
+  @ApiQuery({
+    type: String,
+    name: 'keyword',
+    required: true,
+    example: 'title'
+  })
   @Get('/article')
-  searchArticleByTitleOrContent(@Query('keyword') keyword) {
+  @HttpCode(HttpStatus.OK)
+  searchArticleByTitleOrContent(@Query('keyword') keyword: string) {
     return this.searchService.searchArticleByTitleOrContent(keyword);
   }
 
   /**
    * search article by category
    */
+  @ApiOperation({ summary: 'Search Article by Category' })
+  @ApiQuery({
+    type: String,
+    name: 'label',
+    enum: CategoryType,
+    required: true
+  })
   @Get('/article/category')
-  searchArticleByCategory(@Query('keyword') keyword) {
-    return this.searchService.searchArticleByCategory(keyword);
+  @HttpCode(HttpStatus.OK)
+  searchArticleByCategory(@Query('label') label: CategoryType) {
+    return this.searchService.searchArticleByCategory(label);
   }
 
   /**
    * search article by tag
    */
+  @ApiOperation({ summary: 'Search Article by Tag' })
+  @ApiQuery({
+    type: String,
+    name: 'label',
+    required: true
+  })
   @Get('/article/tag')
-  searchArticleByTag(@Query('keyword') keyword) {
-    return this.searchService.searchArticleByTag(keyword);
+  @HttpCode(HttpStatus.OK)
+  searchArticleByTag(@Query('label') label: string) {
+    return this.searchService.searchArticleByTag(label);
   }
 }
